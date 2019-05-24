5252ae14435e3fafa1321ad4774d0295573a7f77
michalsurowiec <michalsurowiec94@o2.pl>
2019-05-24 15:00:10 +0200

    Ukonczona gra

--- src/michalsurowiec/workshops/GuessNumber.java
+++ src/michalsurowiec/workshops/GuessNumber.java
@@ -1,8 +1,9 @@
 package michalsurowiec.workshops;
 
-import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
+
+//version 1.0.0.
 
 public class GuessNumber {
 
@@ -10,32 +11,28 @@ public class GuessNumber {
 
     public static void main(String[] args) {
         Random rng = new Random();
-        int numberToGuess = rng.nextInt();
+        int numberToGuess = rng.nextInt(100) + 1;
         Scanner console = new Scanner(System.in);
         boolean accomplishment = false;
 
         System.out.println("Cześć! Zapraszam do gry w zgadywanie liczby. \nZasady są proste: spróbuj zgadnąć, jaka liczba została wylosowana od 1 do 100. \nPowodzenia!\n");
 
-        while (accomplishment) {
-            boolean correctness = true;
+        while (!accomplishment) {
             System.out.println("Twoja liczba to:");
-            console.nextInt();
-            try {
+
+            if (console.hasNextInt()) {
                 int answer = console.nextInt();
-            } catch (InputMismatchException ime) {
+                    if (answer == numberToGuess) {
+                        accomplishment = true;
+                        System.out.println("Brawo! Odgadłeś liczbę! To było właśnie " + numberToGuess + " . \nDzięki, że zagrałeś!");
+                    } else if (answer > numberToGuess) {
+                        System.out.println("Za duża liczba! Spróbuj mniejszą");
+                    } else if (answer < numberToGuess) {
+                        System.out.println("Za mała liczba! Spróbuj większą");
+                    }
+            } else {
                 System.out.println("Źle! Miałeś wprowadzić liczbę całkowitą! :(");
-                correctness = false;
-            }
-            int answer = console.nextInt();
-            if (correctness) {
-                if (answer == numberToGuess) {
-                    accomplishment = true;
-                    System.out.println("Brawo! Odgadłeś liczbę! To było właśnie " + numberToGuess + " . \nDzięki, że zagrałeś!");
-                } else if (answer > numberToGuess) {
-                    System.out.println("Za duża liczba! Spróbuj mniejszą");
-                } else  if (answer < numberToGuess) {
-                    System.out.println("Za mała liczba! Spróbuj większą");
-                }
+                String trashBin = console.nextLine();
             }
         }
     }
