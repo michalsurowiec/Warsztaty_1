c8868b2cb3d7732ce45c8be50b8e624678b45e3a
michalsurowiec <michalsurowiec94@o2.pl>
2019-06-20 22:21:08 +0200

    Poprawki do gry w zgadywanie liczb

--- src/michalsurowiec/GuessNumber/GuessNumber.java
+++ src/michalsurowiec/GuessNumber/GuessNumber.java
@@ -11,19 +11,23 @@ public class GuessNumber {
     public static void main(String[] args) {
         Random rng = new Random();
         int numberToGuess = rng.nextInt(100) + 1;
-        Scanner console = new Scanner(System.in);
         boolean accomplishment = false;
 
         System.out.println("Cześć! Zapraszam do gry w zgadywanie liczby. \nZasady są proste: spróbuj zgadnąć, jaka liczba została wylosowana od 1 do 100. \nPowodzenia!\n");
 
+        int stepsQuantity = 0;
         while (!accomplishment) {
+            Scanner console = new Scanner(System.in);
+            stepsQuantity++;
             System.out.println("Twoja liczba to:");
 
             if (console.hasNextInt()) {
                 int answer = console.nextInt();
                     if (answer == numberToGuess) {
                         accomplishment = true;
-                        System.out.println("Brawo! Odgadłeś liczbę! To było właśnie " + numberToGuess + " . \nDzięki, że zagrałeś!");
+                        System.out.println("Brawo! Odgadłeś liczbę! To było właśnie " + numberToGuess + " ." +
+                                "\nUdało ci się odgadnąć liczbę w " + stepsQuantity + " krokach!" +
+                                "\nDzięki, że zagrałeś!");
                     } else if (answer > numberToGuess) {
                         System.out.println("Za duża liczba! Spróbuj mniejszą");
                     } else if (answer < numberToGuess) {
@@ -31,7 +35,6 @@ public class GuessNumber {
                     }
             } else {
                 System.out.println("Źle! Miałeś wprowadzić liczbę całkowitą! :(");
-                String trashBin = console.nextLine();
             }
         }
     }
